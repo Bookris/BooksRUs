be469553b853824ac9111a8dd30d755b27f807e6
hmu1540 <h.mu.1540@gmail.com>
2022-07-31 18:41:00 -0500

    still working on emailField

--- client/archive/Feed.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-export const Feed = () => {
-  return <div> BooksRUs </div>;
-};
---
+++ client/pages/Profile.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+
+export default function Profile() {
+  return (
+    <div> <h1>Profile</h1> </div>
+  )
+}
