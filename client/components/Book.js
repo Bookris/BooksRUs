b994c62216f8120c7c839d974a9ce49306f87988
bprhee01 <73865293+bprhee01@users.noreply.github.com>
2022-07-31 23:57:48 -0400

    updated profile, added book and comment component

--- client/components/Book.js
+++ client/components/Book.js
@@ -1,16 +1,33 @@
 import * as React from 'react';
-
+import Comment from './Comments'
 class Book extends Component {
+  constructor(props){
+    super(props)
+
+  }
   render() {
+    const comments = this.props.book.comments;
+    const commentsComponents = [];
+    for(let i = 0; i< comments.length; i++){
+    const currentComment = comments[i];
+    commentsComponents.push(<Comment comment={currentComment}/>);
+  }
     return (
       <div>
-        <h4>Book Name: </h4>
-        <h4>ISBN: </h4>
+        <h4>Book Name: {this.props.book.name} </h4>
+        <h4>ISBN: {this.props.book.isbn}</h4>
         {/* <h4>: </h4> */}
-        <h4>Description: </h4>
-        <h4>Book Name: </h4>
+        <h4>Description: {this.props.book.description}</h4>
+        <h4>MoreInfo: {this.props.book.moreInfo} </h4>
+        <div>
+          Comments:
+          
+          <div>
+            {commentsComponents}
+          </div>
+        </div>
       </div>
     )
   }
 }
-export default book
+export default Book;
--- client/pages/Profile.js
+++ client/pages/Profile.js
@@ -1,16 +1,25 @@
 import * as React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useStoreState, useStoreActions } from 'easy-peasy';
-
+import Book from '../components/Book';
 export default function Profile() {
   const user = useStoreState((state) => state.user);
+  const likedBooks = user.likedBooks;
+  const likedBookComponents = []
+  for(let i = 0; i< likedBooks.length; i++){
+    const currentBook = likedBooks[i];
+    likedBooksComponents.push(<Book book={currentBook}/>);
+  }
   return (
     <div>
       <h1>Profile</h1>
       <h3>User: {user.username}</h3>
       <h3>Email: {user.email}</h3>
       <h2>--Liked Books--</h2>
-
+      <div>
+        {'where book components will be located'}
+        {likedBooksComponents}
+      </div>
 
 
     </div>
